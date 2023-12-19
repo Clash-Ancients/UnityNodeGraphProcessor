1b93815d4dfa5be0481ad52bfbc1f6df094ead1b
xiaoyulan <15230497577@163.com>
2023-12-19 14:09:11 +0800

    添加node graph processor 资源

--- Assets/com.dialog.nodegraphprocessor/Editor/BaseGraphWindow.cs
+++ Assets/com.dialog.nodegraphprocessor/Editor/BaseGraphWindow.cs
@@ -2,9 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public abstract class BaseGraphWindow : EditorWindow
 {
+    
+    protected VisualElement		rootView;
+    
+    readonly string				graphWindowStyle = "GraphProcessorStyles/BaseGraphView";
+    
     public void InitializeGraph(BaseGraph graph)
     {
         //Initialize will provide the BaseGraphView
@@ -12,4 +18,21 @@ public abstract class BaseGraphWindow : EditorWindow
     }
     
     protected abstract void	InitializeWindow(BaseGraph graph);
+    
+    void InitializeRootView()
+    {
+        rootView = base.rootVisualElement;
+
+        rootView.name = "graphRootView";
+
+        var styleSheet = Resources.Load<StyleSheet>(graphWindowStyle);
+        
+        rootView.styleSheets.Add(styleSheet);
+    }
+
+    protected virtual void OnEnable()
+    {
+        InitializeRootView();
+    }
+
 }
